3cf92c7837df875a8e70c146570c23e552716072
SIlvioVutea <silviovutea@gmail.com>
2024-02-15 12:44:32 +0100

    made all methods to be used

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-Car myCar= new Car(1600,"CV691LM","Audi","A3");
-System.out.println(myCar);
+        Car myCar = new Car(1600, "CV691LM", "Audi", "A3");
+        System.out.println(myCar);
+        myCar.setEngineSize(1800);
+        myCar.setBrand("BMW");
+        myCar.setModel("E Class");
+        myCar.setPlate("LM666HT");
+        System.out.println((myCar.getBrand()));
+        System.out.println((myCar.getModel()));
+        System.out.println((myCar.getEngineSize()));
+        System.out.println((myCar.getPlate()));
+
     }
 }
